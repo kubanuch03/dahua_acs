d70246ea610db9c37491974fe45967b5f97bb5ba
kubanuch03 <kubanuch03@gmail.com>
2025-12-09 14:36:36 +0600

    Stop tracking backend folder

--- backend/app_dahua_sdk/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppDahuaSdkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_dahua_sdk'
--- backend/app_users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from app_users.api.v1.views import  CreateUser
-
-urlpatterns = [
-    path('api/v1/create/', CreateUser.as_view(), name='create-user'),    
-]
-
